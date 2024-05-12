1b91b5d61a27e3e1125df7428dddde7ef87f60bc
Aditya Soni <adityanithariya@gmail.com>
2024-05-13 01:14:08 +0530

    Home Page Redesigned

--- app/page.jsx
+++ app/page.jsx
@@ -1,31 +1,27 @@
-import Image from "next/image";
-import React from "react";
+import Image from 'next/image'
+import React from 'react'
 
-import { CiBellOn, CiSearch, CiUser } from "react-icons/ci";
 import {
-  FaDotCircle,
-  FaFile,
-  FaFolder,
-  FaHeart,
-  FaHome,
-  FaLocationArrow,
-  FaPlus,
-  FaUser,
-  FaVideo,
-} from "react-icons/fa";
-import { IoMdMore } from "react-icons/io";
-import { FaFileShield, FaLocationDot } from "react-icons/fa6";
-import image from "@assets/image.jpg";
-import image1 from "@assets/image1.jpg";
-import image2 from "@assets/image2.jpg";
-import image3 from "@assets/image3.jpg";
-import image4 from "@assets/image4.jpg";
-import image5 from "@assets/image5.jpg";
-import image6 from "@assets/image6.jpg";
+  CiBellOn,
+  CiCircleMore,
+  CiFolderOn,
+  CiSearch,
+  CiUser,
+} from 'react-icons/ci'
+import { FaFile, FaHeart, FaVideo } from 'react-icons/fa'
+import { FaCirclePlus, FaLocationDot } from 'react-icons/fa6'
+import image from '@assets/image.jpg'
+import image1 from '@assets/image1.jpg'
+import image2 from '@assets/image2.jpg'
+import image3 from '@assets/image3.jpg'
+import image4 from '@assets/image4.jpg'
+import image5 from '@assets/image5.jpg'
+import image6 from '@assets/image6.jpg'
+import { GoHome } from 'react-icons/go'
 
 const Home = () => {
   return (
-    <div className=" m-4 flex flex-col gap-5">
+    <div className="my-4 *:mx-4 mb-20 flex flex-col gap-5">
       <div className="flex p-2 text-2xl font-semibold justify-between items-center shadow-sm rounded-sm ">
         <CiUser className="text-blue-800" />
         <div>Home</div>
@@ -36,8 +32,7 @@ const Home = () => {
         <input
           type="text"
           placeholder="search here"
-          className="bg-gray-100 outline-none
-           text-gray-200 font-semibold"
+          className="bg-gray-100 outline-none font-semibold"
         />
       </div>
       <div className="flex justify-around gap-10">
@@ -54,65 +49,55 @@ const Home = () => {
           <FaFile className="text-green-400  text-2xl" />
         </div>
       </div>
-      <div>
-        <h1 className="text-slate-800 text-center font-bold text-2xl mt-4 mb-4">
-          Albums
-        </h1>
-        <div className="grid object-cover overflow-auto grid-cols-2 gap-3 h-[80vh]">
-          <div className="border border-slate-300 rounded p-2">
-            <Image src={image} alt="" />
-            <span>title</span>
-          </div>
-          <div className="border border-slate-300 rounded p-2">
-            <Image src={image1} alt="" />
-            <span>title</span>
-          </div>
-          <div className="border border-slate-300 rounded p-2">
-            <Image src={image2} alt="" />
-            <span>title</span>
-          </div>
-          <div className="border border-slate-300 rounded p-2">
-            <Image src={image3} alt="" />
-            <span>title</span>
-          </div>
-          <div className="border border-slate-300 rounded p-2">
-            <Image src={image4} alt="" />
-            <span>title</span>
-          </div>
-          <div className="border border-slate-300 rounded p-2">
-            <Image src={image5} alt="" />
-            <span>title</span>
-          </div>
-          <div className="border border-slate-300 rounded p-2">
-            <Image src={image6} alt="" />
-            <span>title</span>
-          </div>
+      <div className="grid object-cover overflow-auto grid-cols-2 gap-3 h-[80vh]">
+        <div className="border border-slate-300 rounded p-2">
+          <Image src={image} alt="" />
+          <span>title</span>
+        </div>
+        <div className="border border-slate-300 rounded p-2">
+          <Image src={image1} alt="" />
+          <span>title</span>
+        </div>
+        <div className="border border-slate-300 rounded p-2">
+          <Image src={image2} alt="" />
+          <span>title</span>
+        </div>
+        <div className="border border-slate-300 rounded p-2">
+          <Image src={image3} alt="" />
+          <span>title</span>
+        </div>
+        <div className="border border-slate-300 rounded p-2">
+          <Image src={image4} alt="" />
+          <span>title</span>
+        </div>
+        <div className="border border-slate-300 rounded p-2">
+          <Image src={image5} alt="" />
+          <span>title</span>
+        </div>
+        <div className="border border-slate-300 rounded p-2">
+          <Image src={image6} alt="" />
+          <span>title</span>
         </div>
       </div>
-      <div>
-        <nav
-          className="flex p-3 justify-evenly gap-5  shadow
-        rounded  sticky bottom-0 "
-        >
-          <div className="text-2xl text-yellow-400">
-            <FaHome />
-          </div>
-          <div className="text-2xl">
-            <FaFolder />
-          </div>
-          <div className="text-2xl bg-black rounded-full p-1">
-            <FaPlus className="text-white" />
-          </div>
-          <div className="text-2xl">
-            <FaUser />
-          </div>
-          <div className="text-2xl">
-            <IoMdMore />
-          </div>
-        </nav>
-      </div>
+      <nav className="flex items-center justify-around gap-5 shadow fixed w-full !m-0 py-3 bottom-0 bg-white">
+        <div className="text-2xl text-yellow-400">
+          <GoHome />
+        </div>
+        <div className="text-2xl">
+          <CiFolderOn />
+        </div>
+        <div>
+          <FaCirclePlus className="size-7" />
+        </div>
+        <div className="text-2xl">
+          <CiUser />
+        </div>
+        <div className="text-2xl">
+          <CiCircleMore />
+        </div>
+      </nav>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
